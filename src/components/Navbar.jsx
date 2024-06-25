ad5ca553d172feeac357f1b8b08f49b9293bfd90
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-06-26 04:56:36 +0530

    added home oage

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className="logo">Ris chat</div>
+        <div className="user">
+            <img src="" alt="" />
+            <p>Username</p>
+            <button>Logout</button>
+        </div>
+    </div>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SideBar } from '../components/SideBar'
+import { Chat } from '../components/Chat'
+
+export const Home = () => {
+  return (
+    <div className='home'>
+        <div className="container">
+            <SideBar />
+            <Chat />
+        </div>
+    </div>
+  )
+}
